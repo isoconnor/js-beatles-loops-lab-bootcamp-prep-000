a64f7bea87b5e695c4bd7459e9328794e91a5945
Ian O'Connor <isoconnor89@gmail.com>
2018-03-10 02:47:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,17 +11,18 @@ function theBeatlesPlay(musicians,instruments) {
 
 
 
-function johnLennonFacts(facts){
+function johnLennonFacts(){
   var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
-];
-  var newFacts=[];
-  
-    strings.push(facts[i]+"!!!");    
-  } while (var i=0; i<musicians.length; i++);
-
+   ];
+  var strings=[];
+  var i=0;
+  while (i<facts.length){
+    strings.push(facts[i]+"!!!"); 
+    i++;
+  }
   return strings;
 }
