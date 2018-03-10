aa4cf37d10bd44824ea7a51e2f9d9aec07504aae
Ian O'Connor <isoconnor89@gmail.com>
2018-03-10 02:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians,instruments) {
   var phrases=[];
-  for (var i=0; i<musicians.length, i++){
+  for (var i=0; i<musicians.length; i++){
     phrases.push(musicians[i]+" plays "+instruments[i]);
   }
   return phrases;
